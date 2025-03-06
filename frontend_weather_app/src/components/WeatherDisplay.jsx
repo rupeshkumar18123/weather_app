94469071d6d562506fec9a51751d0373071e0d82
Rupesh kumar <rupesh18123143@gmail.com>
2025-03-06 21:33:36 +0530

    change in ui

--- frontend_weather_app/src/components/WeatherDisplay.jsx
+++ frontend_weather_app/src/components/WeatherDisplay.jsx
@@ -15,7 +15,7 @@ function WeatherDisplay() {
      
 
 
-    <div className="bg-gray-900 text-white p-6 rounded-lg">
+    <div className="bg-gray-900 text-white p-12 rounded-lg">
         <h2 className="text-3xl">{weatherdata.current.name}</h2>
         <p className="text-gray-400">{weatherdata.current.weather[0].description}</p>
         <h3 className="text-5xl">{weatherdata.current.main.temp}°C</h3>
@@ -28,9 +28,9 @@ function WeatherDisplay() {
         </div>
 
         <h3 className="mt-6 text-xl">Forecast</h3>
-        <div className="grid grid-cols-6 gap-4">
+        <div className="grid grid-cols-3 gap-4">
             {weatherdata.forecast.list.slice(0, 6).map((day, index) => (
-                <div key={index} className="bg-gray-800 p-3 rounded-md">
+                <div key={index} className="bg-gray-800 p-12 rounded-md">
                     <p>{new Date(day.dt_txt).toLocaleDateString("en-US", { weekday: "long" })}</p>
                     <p>{Math.round(day.main.temp)}°C</p>
                     <p>{day.weather[0].description}</p>
