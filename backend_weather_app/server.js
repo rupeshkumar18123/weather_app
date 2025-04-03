b4bb20c77d6b6cb1f85283479e5770d73858fec2
Rupesh kumar <rupesh18123143@gmail.com>
2025-04-03 12:33:38 +0530

    add cors in server

--- backend_weather_app/server.js
+++ backend_weather_app/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app =express();
+
+app.use(cors({
+    origin: "*", // Allows requests from any origin
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    
+}));
 app.use(express.json());
 
 const weatherRoute = require("./src/routes/weatherRoutes");
